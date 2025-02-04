dd2bfa9ed4eebf8e1bad1ef6d223f2611b9fa170
7kharo <7kharo@gmail.com>
2025-02-05 00:10:05 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
 const cardList = document.querySelector('.places__list');
 
@@ -7,19 +8,23 @@ const cardList = document.querySelector('.places__list');
 function createCard(cardData, toDeleteCard) {
     const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
     cardItem.querySelector('.card__image').src = cardData.link;
-    cardItem.querySelector('card__title').textContent = cardData.name;
+    cardItem.querySelector('.card__title').textContent = cardData.name;
     cardItem.querySelector('.card__delete-button').addEventListener('click', toDeleteCard);
     return cardItem;
 }
+
 // @todo: Функция удаления карточки
 function toDeleteCard (event) {
-    const removedCard = event.target.parentElement;
-    removedCard.remove();
+    event.target.parentElement.remove(); // получаем ссылку на родительский элемент кнопки удаления (карточку) и удаляем выбранный класс
 }
+
 // @todo: Функция добавления карточки
 function addCard (cardList, card) {
     cardList.append(card);
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(element => {addCard(cardList, card)});
+initialCards.forEach(cardsItem => {
+    const newCard = createCard(cardsItem, toDeleteCard);
+    addCard(cardList, newCard);
+});
